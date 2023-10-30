fc2c438756d50f85f83e6e643f6f44464e4319a7
Mikhail Vialov <mikhail_vialov@epam.com>
2023-10-31 00:18:37 +0100

    api update

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://mgwwj8xgob.execute-api.eu-central-1.amazonaws.com/api",
+  import: "https://sa36n4404c.execute-api.eu-central-1.amazonaws.com/api",
   bff: "https://rg05khpmnc.execute-api.eu-central-1.amazonaws.com/api",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
